1fcc8174b8a175aef43ea4e605bf73452a306e08
mcshell7 <mishakhamardiuk@gmail.com>
2024-12-17 15:12:07 -0500

    hw1 commit

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-
 const http = require('node:http');
+const path = require('node:path');
 
 
+
+//
 const LiveServer = async () => {
 
-// Create a local server to receive data from
-    const server = http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            data: 'Hello World!',
-        }));
-    });
+// http Create a local server to receive data from
+//     const server = http.createServer((req, res) => {
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         const ulr = req.url;
+//         if (url === '/about') {
+//             switch (req.method) {
+//                 case 'GET':
+//                     return res.end(
+//                         JSON.stringify({data: 'About Page'})
+//                     );
+//                 case ' POST':
+//                     return res.end(
+//                         JSON.stringify({data: 'About Page'})
+//                     );
+//             }
+//         }
+//         if (url === '/contact') {
+//             return res.end(JSON.stringify({
+//                 data: 'Contact Page'
+//             }));
+//         }
+//     });
+//     server.listen(8000);
 
-    server.listen(8000);
 }
-
 void LiveServer();
+
+
