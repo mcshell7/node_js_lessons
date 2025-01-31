2b444071ff02351e648185ff709afd56bf6442aa
mcshell7 <mishakhamardiuk@gmail.com>
2025-01-31 11:20:17 -0500

    lesson2 path

--- main.js
+++ main.js
@@ -1,53 +1,21 @@
-
 const path = require('node:path');
-const readline = require("node:readline");
-const fs = require('fs/promises');
-
+const fs = require('node:fs/promises');
 
 const foo = async () => {
+    const pathToFile = path.join(__dirname, 'baseFolder');
+    await fs.mkdir(pathToFile, {recursive: true});
+    const folders = ['folder1','folder2','folder3','folder4','folder5'];
+    const files = ['file1.txt','file2.txt','file3.txt','file4.txt','file5.txt'];
 
-    // path
-//     const pathToHelper = path.join(__dirname, 'helpers', 'helper.js');
-//     console.log(path.basename(pathToHelper));
-//     console.log(path.dirname(pathToHelper));
-//     console.log(path.extname(pathToHelper));
-//     console.log(path.parse(pathToHelper));
-//     console.log(path.normalize(pathToHelper));
-//     console.log(path.isAbsolute(pathToHelper));
+    await Promise.all(folders.map(async (folder) => {
+        const folderPath = path.join(pathToFile, folder);
+        await fs.mkdir(folderPath, {recursive: true});
 
-
-    // readline
-    const rlInstance = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-
-    rlInstance.question('What is your name? ', (name ) => {
-        console.log(`Hello ${name}`);
-        rlInstance.close();
-    })
-// os
-    const os = require('os');
-
-    console.log('Архитектура:', os.arch());
-    console.log('Платформа:', os.platform());
-    console.log('Тип ОС:', os.type());
-    console.log('Версия ОС:', os.release());
-    console.log('Имя хоста:', os.hostname());
-    console.log('Домашняя директория:', os.homedir());
-    console.log('Временная директория:', os.tmpdir());
-
-// fs
-    const fsPromises = require('fs/promises');
-
-    await fsPromises.writeFile('test.txt', 'Hello world');
-    const data = await fsPromises.readFile(path.join(process.cwd(), 'test.txt'), 'utf8');
-    console.log(data);
-
-
+        await Promise.all(files.map(async (file) => {
+            await fs.writeFile(path.join(folderPath, file), 'Hello world');
+        }) )
+    }));
 }
-
-
 
 void foo();
 
